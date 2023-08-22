eba88958612528d800c71c41164f7bcc614e612b
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-08-22 11:49:00 +0100

    added tmm logger

--- src/fabmos/transport/tmm.py
+++ src/fabmos/transport/tmm.py
@@ -6,6 +6,10 @@ def create_domain(path: str) -> pygetm.domain.Domain:
     return pygetm.domain.create_spherical(lon=lon, lat=lat, mask=mask, H=H)
 
 class Simulator(simulator.Simulator):
+    def __init__(self, domain: pygetm.domain.Domain, fabm_config: str = "fabm.yaml"):
+        super().__init__(domain, fabm_config)
+        self.tmm_logger = self.logger.getChild("tmm")
+
     def transport(self, timestep: float):
         for tracer in self.tracers:
             #update tracer.all_values in place
