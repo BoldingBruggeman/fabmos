98c1b37c85040504a6b63999208a745a6dee2e6b
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-10-27 15:11:24 +0100

    cleaned up conda recipe

--- conda.recipe/build.sh
+++
@@ -1,7 +0,0 @@
-mkdir build
-cd build
-cmake -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release $SRC_DIR/python
-cmake --build . --config Release --parallel $CPU_COUNT
-cmake --install .
-cd ..
-rm -r build
