26fa336e3d3ef4760a98d0fe4033a64424944782
Rui Ying <38958822+ruiying-ocean@users.noreply.github.com>
2025-09-11 15:50:08 +0100

    support loadmat providing sparse matrix in COO format in scipy >= 1.15 (#23)
    * fix bug which happens when .mat data contains a sparse matrix in COO
    
    * a cleaner solution proposed by @jornbr
    
    * correct comment (CSC format)

--- src/fabmos/transport/tmm.py
+++ src/fabmos/transport/tmm.py
@@ -219,7 +219,8 @@ class TransportMatrix(pygetm.input.LazyArray):
         except OSError:
             # MATLAB < 7.3 format
             vardict = scipy.io.loadmat(fn, variable_names=(self._group_name,))
-            A = vardict[self._group_name]
+            ## Convert COO matrix to CSC format if needed
+            A = vardict[self._group_name].tocsc()                
             A_ir = A.indices
             A_jc = A.indptr
             dtype = A.dtype
